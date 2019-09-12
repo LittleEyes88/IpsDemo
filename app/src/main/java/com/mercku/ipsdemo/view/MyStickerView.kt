5be84dc23cd9e231e024a1a1a44691edf3fb5c34
Li Jia <pexjl12310@163.com>
2019-09-12 17:28:57 +0800

    fix scale bugs

--- app/src/main/java/com/mercku/ipsdemo/view/MyStickerView.kt
+++ app/src/main/java/com/mercku/ipsdemo/view/MyStickerView.kt
@@ -1,42 +1,11 @@
 package com.mercku.ipsdemo.view
 
-import android.app.AlertDialog
+
 import android.content.Context
-import android.content.DialogInterface
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Matrix
-import android.graphics.Paint
-import android.graphics.Path
-import android.graphics.Point
-import android.graphics.PointF
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffXfermode
-import android.graphics.Region
-import android.net.Uri
-import android.text.TextUtils
+import android.graphics.*
 import android.util.AttributeSet
-import android.util.Log
-import android.view.MotionEvent
-import android.view.ScaleGestureDetector
-import android.widget.EditText
-import android.widget.Toast
-
-
 import com.mercku.ipsdemo.R
-import com.mercku.ipsdemo.model.FreeRoom
-import com.mercku.ipsdemo.model.IntersectionArea
-import com.mercku.ipsdemo.model.IpsHouse
-import com.mercku.ipsdemo.model.Node
-import com.mercku.ipsdemo.util.BitmapUtil
 import com.mercku.ipsdemo.util.MathUtil
-import com.mercku.ipsdemo.view.BaseEditView.Companion.DEFAULT_DOT_RADIUS
-import com.mercku.ipsdemo.view.BaseEditView.Companion.NONE_TOUCH
-import java.io.File
-
-import java.util.ArrayList
 
 /**
  * Created by yanqiong.ran on 2019-07-19.
@@ -65,12 +34,12 @@ class MyStickerView : BaseEditView {
         var points = drawAllLocator(canvas)
 
         //get sticker's location
-        var center = MathUtil.getPolygenCenter(points)
-        var curX = width / 2.0f
-        var curY = height / 2.0f
+        var center = MathUtil.getCenterOfPoint(points)
+        var curX = center.x
+        var curY = center.y
 
         var stickerPoint = PointF(curX, curY)
-        drawLineWithSticker(stickerPoint, canvas)
+        drawLineWithSticker(stickerPoint, points, canvas)
         drawSticker(stickerPoint, canvas)
 
     }
@@ -85,36 +54,61 @@ class MyStickerView : BaseEditView {
         canvas.drawBitmap(stickerBitmp, matrix, paint)
     }
 
-    private fun drawLineWithSticker(curPoint: PointF, canvas: Canvas) {
-        var index = 0
-        var unit = mHouseDetail!!.mBitmapActualWidth / (mHouseBitmap!!.width * mTotalScaled)
-        var curDisX = curPoint.x * unit
-        var curDisY = curPoint.y * unit
-        var startX = getImgLeftAfterTransOrScale()
-        var startY = getImgTopAfterTransOrScale()
-        while (index < mHouseDetail!!.mData!!.size) {
-            var locator = mHouseDetail!!.mData!![index]
-            if (locator.mIsSelected || locator.mIsAdded) {
-                var nextX = startX + mHouseBitmap!!.width * locator.mLocationActual.x * mTotalScaled
-                var nextDisX = nextX * unit
-                var nextY = startY + mHouseBitmap!!.height * locator.mLocationActual.y * mTotalScaled
-                var nextDisY = nextY * unit
-                var actualDis = MathUtil.distance(curDisX, curDisY, nextDisX, nextDisY)
-                var pixDis = MathUtil.distance(curPoint.x, curPoint.y, nextX, nextY)
-
-                canvas.drawLine(curPoint.x, curPoint.y, nextX, nextY, mLinePaint)
-
-                var path = Path()
-                path.moveTo(curPoint.x, curPoint.y)
-                path.lineTo(nextX, nextY)
-                //距离有问题，可能因为坐标错误
-                var disStr: String = String.format("%.1f", actualDis)
-                canvas.drawTextOnPath(disStr + "m", path, (pixDis / 2).toFloat(), 0f, mTextPaint)
-            }
-            index++
-
+    private fun drawLineWithSticker(curPoint: PointF, positionList: ArrayList<PointF>?, canvas: Canvas) {
+        if (positionList.isNullOrEmpty()) {
+            return
         }
+        val unit = mHouseDetail!!.mBitmapActualWidth / (mHouseBitmap!!.width * mTotalScaled)
+        val curX = curPoint.x
+        val curY = curPoint.y
+        val curDisX = curPoint.x * unit
+        val curDisY = curPoint.y * unit
+        for (nextIndex in 0 until positionList.size) {
+            val nextPoint = positionList[nextIndex]
+            val nextX = nextPoint.x
+            val nextY = nextPoint.y
+            val nextDisX = nextPoint.x * unit
+            val nextDisY = nextPoint.y * unit
+            val actualDis = MathUtil.distance(curDisX, curDisY, nextDisX, nextDisY)
+            val pixDis = MathUtil.distance(curX, curY, nextX, nextY)
+
+            canvas.drawLine(curX, curY, nextX, nextY, mLinePaint)
+            val path = Path()
+            path.moveTo(curX, curY)
+            path.lineTo(nextX, nextY)
+            val disStr: String = String.format("%.1f", actualDis)
+            canvas.drawTextOnPath(disStr + "m", path, pixDis / 2, 0f, mTextPaint)
+        }
+//        var index = 0
+//        var unit = mHouseDetail!!.mBitmapActualWidth / (mHouseBitmap!!.width * mTotalScaled)
+//        var curDisX = curPoint.x * unit
+//        var curDisY = curPoint.y * unit
+//        var startX = getImgLeftAfterTransOrScale()
+//        var startY = getImgTopAfterTransOrScale()
+//        while (index < mHouseDetail!!.mData!!.size) {
+//            var locator = mHouseDetail!!.mData!![index]
+//            if (locator.mIsSelected || locator.mIsAdded) {
+//                var nextX = startX + mHouseBitmap!!.width * locator.mLocationActual.x * mTotalScaled
+//                var nextDisX = nextX * unit
+//                var nextY = startY + mHouseBitmap!!.height * locator.mLocationActual.y * mTotalScaled
+//                var nextDisY = nextY * unit
+//                var actualDis = MathUtil.distance(curDisX, curDisY, nextDisX, nextDisY)
+//                var pixDis = MathUtil.distance(curPoint.x, curPoint.y, nextX, nextY)
+//
+//                canvas.drawLine(curPoint.x, curPoint.y, nextX, nextY, mLinePaint)
+//
+//                var path = Path()
+//                path.moveTo(curPoint.x, curPoint.y)
+//                path.lineTo(nextX, nextY)
+//                //距离有问题，可能因为坐标错误
+//                var disStr: String = String.format("%.1f", actualDis)
+//                canvas.drawTextOnPath(disStr + "m", path, (pixDis / 2).toFloat(), 0f, mTextPaint)
+//            }
+//            index++
+//
+//        }
     }
+
     override fun getImgInitialLeft(): Float {
         var left = (width / 2.0f - mHouseBitmap!!.width / 2.0f)
         android.util.Log.d("ryq", " getImgInitialLeft left=" + left)
